ba04c949ad374b1d7b47d6ec17f659414f85592e
Aldanis Vigo <aldanisvigo@gmail.com>
2022-07-03 20:44:52 -0500

    Initial Commit

---
+++ Shared/SwiftUI_Tuturial_RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tuturial_RestartApp.swift
+//  Shared
+//
+//  Created by Aldanis Vigo on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tuturial_RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
